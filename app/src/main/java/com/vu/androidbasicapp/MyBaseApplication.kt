b78d76e6c2086942a5806c4448e5f6be8fbd04ea
JustinCoKA <ausjustin12@gmail.com>
2026-05-17 11:15:06 +1000

    Apply Hilt application annotation

--- app/src/main/java/com/vu/androidbasicapp/MyBaseApplication.kt
+++ app/src/main/java/com/vu/androidbasicapp/MyBaseApplication.kt
@@ -1,12 +1,7 @@
 package com.vu.androidbasicapp
 
 import android.app.Application
-import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
-class MyBaseApplication: Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.d("nit3213", "Application onCreate was called")
-    }
-}
+@HiltAndroidApp
+class MyBaseApplication : Application()
